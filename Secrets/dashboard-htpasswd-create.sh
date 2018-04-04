1bc3f0dad129c51583d5c6b58c4e9231b5a0daaf
Dillon Courts <dillon.courts@gmail.com>
2018-04-04 10:46:58 -0400

    Bug fixes r us

--- Secrets/dashboard-htpasswd-create.sh
+++ Secrets/dashboard-htpasswd-create.sh
@@ -3,5 +3,5 @@
 
 AUTH_USERNAME=$(echo -n ${KUBERNETES_DASHBOARD_USERNAME})
 AUTH_PASSWORD=$(openssl passwd -apr1 $KUBERNETES_DASHBOARD_PASSWORD)
-AUTH_PLACEHOLDER="$AUTH_USERNAME:$AUTH_PASSWORD"
+AUTH_PLACEHOLDER=$(echo "$AUTH_USERNAME:$AUTH_PASSWORD"  | base64)
 sed -e "s#{{auth_placeholder}}#${AUTH_PLACEHOLDER}#g" ./dashboard-htpasswd-template.yml > dashboard-htpasswd.yml
